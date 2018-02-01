d6182a3e8bfae3aef4115ada0e9f1f867dcab242
surabhi8 <surabhigupta2442@gmail.com>
2018-02-01 22:47:32 +0530

    For Ex5, test case for right response

--- Exercise5/test/server.test.js
+++ Exercise5/test/server.test.js
@@ -1,5 +1,6 @@
 
 const server = require('/Users/surabhigupta/makemehapi/Exercise5/solution/server.js')
+const fs = require('fs')
 describe('Rendering templates',()=>{
     test('Should return right status code',(done)=>{
         const options = {
@@ -22,4 +23,15 @@ describe('Rendering templates',()=>{
             done();
         });
         })
+        test('Should return right response',(done)=>{
+            const options = {
+                method:'GET',
+                url : 'http://localhost:8080?name=Handling'
+            }   
+            let result = fs.readFileSync('/Users/surabhigupta/makemehapi/Exercise5/solution/templates/expected.html')
+            server.inject(options,(response)=>{
+                expect(response.result).toBe(result.toString());
+                done();
+            })
+});
 });
