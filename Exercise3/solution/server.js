a781eb62e9bd1adf8f38ce2d7068ad9a7b93bfcf
surabhi8 <surabhigupta2442@gmail.com>
2018-02-01 15:03:08 +0530

    server started and running

---
+++ Exercise3/solution/server.js
@@ -0,0 +1,23 @@
+
+const Hapi =  require('hapi')
+
+const server = new  Hapi.Server
+server.connection({
+    host: '127.0.0.1',
+    port: 8080,
+  });
+
+
+
+if(!module.parent){
+    server.start ( (err) => {
+        if(err){
+            throw err;
+        } 
+        console.log("Serever started")
+    }
+)
+}
+
+
+module.exports = server
