6185f9f79d557c81ce1d796af857b89895b87f43
surabhi8 <surabhigupta2442@gmail.com>
2018-02-01 21:20:58 +0530

    For exercise 5, Invalid request test case

--- Exercise4/test/server.test.js
+++ Exercise4/test/server.test.js
@@ -25,4 +25,27 @@ describe('Routes',()=>{
             done();
         });
         })
+
+    test('Should return right response',(done)=>{
+        const options = {
+            method:'GET',
+            url : '/foo/bar/baz/file.html'
+        }   
+        server.inject(options,(response)=>{
+            let result = fs.readFileSync('/Users/surabhigupta/makemehapi/Exercise4/solution/public/file.html')
+            expect(response.result).toBe(result.toString());
+            done();
+        });
+        })
+        
+    test('Invalid request',(done)=>{
+        const options = {
+            method:'GET',
+            url : '/'
+        }   
+        server.inject(options,(response)=>{
+            expect(response.statusCode).toBe(404);
+            done();
+        });
+        })
     });
