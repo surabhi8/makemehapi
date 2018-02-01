0233d63f198e35933a86a6e4ad6a163555a22904
surabhi8 <surabhigupta2442@gmail.com>
2018-02-01 21:17:07 +0530

    For exercise 4, test case for correct response

--- Exercise4/test/server.test.js
+++ Exercise4/test/server.test.js
@@ -1,6 +1,7 @@
 
 const server = require('/Users/surabhigupta/makemehapi/Exercise4/solution/server.js')
 const fs = require('fs')
+let readDirFiles = require('read-dir-files');
 describe('Routes',()=>{
     test('Should return right status code',(done)=>{
         const options = {
@@ -13,4 +14,15 @@ describe('Routes',()=>{
         })
        
     })
-})
+    test('Should return right response',(done)=>{
+        const options = {
+            method:'GET',
+            url : '/foo/bar/baz/file.html'
+        }   
+        server.inject(options,(response)=>{
+            let result = fs.readFileSync('/Users/surabhigupta/makemehapi/Exercise4/solution/public/file.html')
+            expect(response.result).toBe(result.toString());
+            done();
+        });
+        })
+    });
